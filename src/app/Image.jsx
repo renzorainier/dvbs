986734b30f21f250b780966b2d328042204b2d47
renzorainier <114910204+renzorainier@users.noreply.github.com>
2024-06-06 08:12:00 +0000

    Thu, Jun 6, 2024, 8:12 AM +00:00

--- src/app/Image.jsx
+++ src/app/Image.jsx
@@ -3,17 +3,15 @@ import Image from "next/image";
 
 const BobbingImage = () => {
   return (
-    <div className="flex items-start justify-center h-screen">
-      <div className="h-96 w-96 flex items-center justify-center animate-bob" style={{ transform: 'translateY(-50px)' }}>
-        <div className="relative h-80 w-80">
-          <Image
-            src="/firefighter.png"
-            alt="Bobbing Image"
-            layout="responsive" // Change the layout prop value to "responsive"
-            width={80} // Set the width and height explicitly
-            height={80}
-          />
-        </div>
+    <div className="absolute inset-0 flex items-center justify-center animate-bob z-0">
+      <div className="relative h-80 w-80">
+        <Image
+          src="/firefighter.png"
+          alt="Bobbing Image"
+          layout="responsive"
+          width={80}
+          height={80}
+        />
       </div>
     </div>
   );
