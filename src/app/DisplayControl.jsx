535d011c4399cbb8d0f75e844d64aa414d9ef983
renzorainier <114910204+renzorainier@users.noreply.github.com>
2024-06-06 04:52:59 +0000

    Thu, Jun 6, 2024, 4:52 AM +00:00

--- src/app/DisplayControl.jsx
+++ src/app/DisplayControl.jsx
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { collection, onSnapshot, doc } from "firebase/firestore";
+import { db2 } from "./firebaseConfig2.js";
 import AttendanceChart from "./AttendanceChart";
 import PointingSystemGraph from "./PointingSystemGraph";
 import Password from "./Password.jsx";
@@ -10,6 +12,21 @@ import { BsGraphUpArrow } from "react-icons/bs";
 function DisplayControl() {
   const [currentComponent, setCurrentComponent] = useState(null);
   const [isVisitorView, setIsVisitorView] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const docRef = doc(db, "points", "monitor1");
+      const docSnap = await getDoc(docRef);
+
+      if (docSnap.exists()) {
+        console.log("Document data:", docSnap.data());
+      } else {
+        console.log("No such document!");
+      }
+    };
+
+    fetchData();
+  }, []);
 
   const handleButtonClick = (componentName) => {
     setCurrentComponent(componentName);
@@ -33,9 +50,7 @@ function DisplayControl() {
       default:
         return (
           <div className="flex flex-col justify-center items-center h-screen">
-            <div className="container mx-auto">
-             
-            </div>
+            <div className="container mx-auto"></div>
           </div>
         );
     }
