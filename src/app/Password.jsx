97348922c89f9b71667bfcafea949ff138445612
renzorainier <114910204+renzorainier@users.noreply.github.com>
2024-06-06 05:25:06 +0000

    Thu, Jun 6, 2024, 5:25 AM +00:00

--- src/app/Password.jsx
+++ src/app/Password.jsx
@@ -2,8 +2,6 @@ import React, { useState } from "react";
 
 import { FaLock } from "react-icons/fa6";
 
-
-
 const Password = ({
   correctPassword,
   children,
@@ -35,44 +33,42 @@ const Password = ({
   };
 
   return (
-    <div >
+    <div>
       {isAuthenticated || isVisitorView ? (
         children
       ) : (
         <div className="min-h-screen flex items-center justify-center bg-gray-100">
-
-        <div className="bg-white rounded-lg shadow-2xl p-8 max-w-md w-full">
-          <h1 className="text-2xl font-semibold text-gray-800 mb-6 text-center">
-            Restricted Access <FaLock />
-
-          </h1>
-          <p className="text-gray-600 mb-6 text-center">
-            This feature is intended for authorized users. Enter the password to
-            edit data or view as a visitor.
-          </p>
-          <form onSubmit={handleSubmit}>
-            <input
-              type="number"
-              value={pin}
-              onChange={handlePinChange}
-              className="border border-gray-300 rounded-lg px-4 py-3 mb-4 w-full text-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-              placeholder="Enter PIN"
+          <div className="bg-white rounded-lg shadow-2xl p-8 max-w-md w-full">
+            <h1 className="text-2xl font-semibold text-gray-800 mb-6 text-center flex items-center justify-center">
+              Restricted Access <FaLock className="ml-2" />
+            </h1>
+            <p className="text-gray-600 mb-6 text-center">
+              This feature is intended for authorized users. Enter the password
+              to edit data or view as a visitor.
+            </p>
+            <form onSubmit={handleSubmit}>
+              <input
+                type="number"
+                value={pin}
+                onChange={handlePinChange}
+                className="border border-gray-300 rounded-lg px-4 py-3 mb-4 w-full text-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+                placeholder="Enter PIN"
               />
-            {error && <p className="text-red-500 text-sm mb-4">{error}</p>}
-            <button
-              type="submit"
-              className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-3 px-4 rounded-lg w-full transition duration-200">
-              Submit
-            </button>
-            <button
-              onClick={handleVisitorView}
-              type="button"
-              className="bg-gray-500 hover:bg-gray-600 text-white font-semibold py-3 px-4 rounded-lg w-full mt-4 transition duration-200">
-              Enter as Visitor
-            </button>
-          </form>
+              {error && <p className="text-red-500 text-sm mb-4">{error}</p>}
+              <button
+                type="submit"
+                className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-3 px-4 rounded-lg w-full transition duration-200">
+                Submit
+              </button>
+              <button
+                onClick={handleVisitorView}
+                type="button"
+                className="bg-gray-500 hover:bg-gray-600 text-white font-semibold py-3 px-4 rounded-lg w-full mt-4 transition duration-200">
+                Enter as Visitor
+              </button>
+            </form>
+          </div>
         </div>
-                </div>
       )}
     </div>
   );
