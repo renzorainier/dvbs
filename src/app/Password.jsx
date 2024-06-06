07170738d3cb360828c7ffd56ba1cbabdc701ad2
renzorainier <114910204+renzorainier@users.noreply.github.com>
2024-06-06 05:40:14 +0000

    Thu, Jun 6, 2024, 5:40 AM +00:00

--- src/app/Password.jsx
+++ src/app/Password.jsx
@@ -37,7 +37,7 @@ const Password = ({
       {isAuthenticated || isVisitorView ? (
         children
       ) : (
-        <div className="min-h-screen flex items-center justify-center bg-gray-100">
+        <div className="min-h-screen flex items-center justify-center bg-gray-400">
           <div className="bg-white rounded-lg shadow-2xl p-8 max-w-md w-full">
             <h1 className="text-2xl font-semibold text-gray-800 mb-6 text-center flex items-center justify-center">
               Restricted Access <FaLock className="ml-2" />
