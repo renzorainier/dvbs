824b5a581dd7207d285caf0dda6b405d09c97e56
renzorainier <114910204+renzorainier@users.noreply.github.com>
2024-06-07 16:09:27 +0000

    Fri, Jun 7, 2024, 4:09 PM +00:00

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ import Main from "./Main.jsx";
 import Head from "next/head.js";
 import React, { useState, Fragment } from "react";
 
+
 export default function Home() {
   return (
     <>
@@ -14,6 +15,12 @@ export default function Home() {
           href="https://fonts.googleapis.com/css2?family=Urbanist:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400&amp;display=swap"
           rel="stylesheet"
         />
+        {/* Open Graph meta tags */}
+        <meta property="og:title" content="Rescue Zone | DVBS 2024" />
+        <meta property="og:description" content="Manage attendance efficiently with our app." />
+        <meta property="og:image" content="https://example.com/your-image.jpg" />
+        <meta property="og:url" content="https://example.com" />
+        {/* Add more Open Graph meta tags as needed */}
       </Head>
 
       <div>
