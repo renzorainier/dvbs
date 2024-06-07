46a08aa906175d5fe293505633c11cf390e950ca
renzorainier <114910204+renzorainier@users.noreply.github.com>
2024-06-07 01:30:21 +0000

    Fri, Jun 7, 2024, 1:30 AM +00:00

--- src/app/Main.jsx
+++ src/app/Main.jsx
@@ -112,7 +112,7 @@ function Main() {
             <div className="container mx-auto relative z-10">
               <div className="grid grid-cols-2 md:grid-cols-2 gap-2">
                 <button
-                  className="focus:outline-none bg-white-500 backdrop-blur-5xl  text-white font-semibold py-4 px-6 rounded-lg shadow-lg transition duration-300 transform hover:scale-105"
+                  className="focus:outline-none bg-white-500  text-white font-semibold py-4 px-6 rounded-lg shadow-lg transition duration-300 transform hover:scale-105"
                   onClick={() => handleButtonClick("Tab")}
                   style={{ animation: "slide-from-left 1s ease forwards" }}>
                   <div
@@ -187,7 +187,7 @@ function Main() {
                 </button>
 
                 <button
-                  className="focus:outline-none bg-white-500 backdrop-blur-5xl text-white font-semibold py-4 px-6 rounded-lg shadow-lg transition duration-300 transform hover:scale-105"
+                  className="focus:outline-none bg-white-500   backdrop-blur-5xl text-white font-semibold py-4 px-6 rounded-lg shadow-lg transition duration-300 transform hover:scale-105"
                   onClick={() => handleButtonClick("SalvationDecision")}
                   style={{ animation: "slide-from-left 1s ease forwards" }}>
                   <div
