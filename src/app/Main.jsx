b756c65f1e8f8f2b83ca2adf9bcdd99ae30b94a5
renzorainier <114910204+renzorainier@users.noreply.github.com>
2024-06-07 02:35:59 +0000

    Fri, Jun 7, 2024, 2:35 AM +00:00

--- src/app/Main.jsx
+++ src/app/Main.jsx
@@ -106,15 +106,13 @@ function Main() {
             }}>
             <div className="text-white text-center mb-10 relative z-10">
               <h1 className="font-bold text-9xl bg-gradient-to-r from-yellow-500 via-red-500 to-orange-500 text-transparent bg-clip-text ">
-              Rescue
+                Rescue
               </h1>
-              <h1 className="font-bold text-9xl bg-gradient-to-r from-red-500 via-yellow-500 to-orange-500 text-transparent bg-clip-text ">
-              Zone
+              <h1 className="font-bold text-9xl bg-gradient-to-r from-yellow-500 via-red-500 to-yellow-500 text-transparent bg-clip-text ">
+                Zone
               </h1>
               <h2 className="text-3xl font-bold ">2024</h2>
-              <h3 className="text-5xl font-semibold ">
-                Rescue&nbsp; Zone
-              </h3>
+              <h3 className="text-5xl font-semibold ">Rescue&nbsp; Zone</h3>
             </div>
 
             <div className="container mx-auto relative z-10">
