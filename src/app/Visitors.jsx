73d5741d066a2f0747a910885b8ff4f037ec15f4
renzorainier <114910204+renzorainier@users.noreply.github.com>
2024-06-08 16:50:47 +0000

    Sat, Jun 8, 2024, 4:50 PM +00:00

--- src/app/Visitors.jsx
+++ src/app/Visitors.jsx
@@ -310,46 +310,47 @@ function Visitors({ config, currentConfigIndex, setCurrentConfigIndex, isVisitor
                 placeholder="Address/Select Route"
                 className="border border-gray-300 rounded-lg px-4 py-2 w-full focus:outline-none focus:border-[${config.color}]"
               />
-              <Menu as="div" className="relative w-1/2">
-                <div>
-                  <Menu.Button
-                    className={`bg-[${config.color}] flex justify-center text-white text-center inline-flex justify-between w-full rounded-md shadow-sm px-4 py-2 text-xl font-bold text-gray-700`}>
-                    {"Route"}
-                    <ChevronDownIcon
-                      className="ml-2 -mr-1 h-7 w-7"
-                      aria-hidden="true"
-                    />
-                  </Menu.Button>
-                </div>
-                <Transition
-                  as={Fragment}
-                  enter="transition ease-out duration-100"
-                  enterFrom="transform opacity-0 scale-95"
-                  enterTo="transform opacity-100 scale-100"
-                  leave="transition ease-in duration-75"
-                  leaveFrom="transform opacity-100 scale-100"
-                  leaveTo="transform opacity-0 scale-95">
-                  <Menu.Items className="origin-top-right absolute z-50 right-0 mt-2 w-full rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                    <div className="py-1">
-                      {predefinedRoutes.map((route) => (
-                        <Menu.Item key={route}>
-                          {({ active }) => (
-                            <button
-                              onClick={() => handleRouteSelect(route)}
-                              className={`${
-                                active
-                                  ? "bg-gray-100 text-gray-900"
-                                  : "text-gray-700"
-                              } block w-full text-left px-4 py-2 text-lg font-medium`}>
-                              {route}
-                            </button>
-                          )}
-                        </Menu.Item>
-                      ))}
-                    </div>
-                  </Menu.Items>
-                </Transition>
-              </Menu>
+    <Menu as="div" className="relative w-1/2">
+  <div>
+    <Menu.Button
+      className={`bg-[${config.color}] flex justify-center text-white text-center inline-flex justify-between w-full rounded-md shadow-sm px-4 py-2 text-xl font-bold text-gray-700`}>
+      {"Route"}
+      <ChevronDownIcon
+        className="ml-2 -mr-1 h-7 w-7"
+        aria-hidden="true"
+      />
+    </Menu.Button>
+  </div>
+  <Transition
+    as={Fragment}
+    enter="transition ease-out duration-100"
+    enterFrom="transform opacity-0 scale-95"
+    enterTo="transform opacity-100 scale-100"
+    leave="transition ease-in duration-75"
+    leaveFrom="transform opacity-100 scale-100"
+    leaveTo="transform opacity-0 scale-95">
+    <Menu.Items className="origin-top-right absolute z-50 right-0 mt-2 w-full rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+      <div className="py-1 grid grid-cols-2 gap-2">
+        {predefinedRoutes.map((route) => (
+          <Menu.Item key={route}>
+            {({ active }) => (
+              <button
+                onClick={() => handleRouteSelect(route)}
+                className={`${
+                  active
+                    ? "bg-gray-100 text-gray-900"
+                    : "text-gray-700"
+                } block w-full text-left px-4 py-2 text-lg font-medium`}>
+                {route}
+              </button>
+            )}
+          </Menu.Item>
+        ))}
+      </div>
+    </Menu.Items>
+  </Transition>
+</Menu>
+
             </div>
 
             <div className="flex items-center">
