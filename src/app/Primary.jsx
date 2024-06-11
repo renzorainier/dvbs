e843fb33936a3a64bd4cf1d412d2aef434f23d96
renzorainier <114910204+renzorainier@users.noreply.github.com>
2024-06-11 09:32:02 +0000

    Tue, Jun 11, 2024, 9:32 AM +00:00

--- src/app/Primary.jsx
+++ src/app/Primary.jsx
@@ -240,10 +240,11 @@ function Primary({
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
-    const strippedName = name.replace(selectedStudentInfo.id, '');
+    console.log(`Name: ${name}`);  // Log the name to the console
+    // const strippedName = name.replace(selectedStudentInfo.id, '');
     setEditableStudentInfo((prevState) => ({
       ...prevState,
-      [strippedName]: Number(value),
+      [name]: Number(value),
     }));
   };
 
